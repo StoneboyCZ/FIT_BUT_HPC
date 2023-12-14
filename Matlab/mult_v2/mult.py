9939b3788a0154db865085cf4745950bf64e1170
Petr Veigend <iveigend@fit.vut.cz>
2023-12-14 17:57:56 +0100

    multiplication

--- Matlab/mult_v2/mult.py
+++ Matlab/mult_v2/mult.py
@@ -1,5 +1,5 @@
 ''' PROBLEM
-y' = cos(t)/exp(t); y(0) = 1; TMAX = 1; h = 0.1
+y' = cos(t)*exp(t); y(0) = 1; TMAX = 1; h = 0.1
 
 ODE
 y1' = y5 y1(0) = 1
@@ -8,7 +8,7 @@ y3' = y2 y3(0) = 0 sin(t)
 y4' = y4 y4(0) = 1 e^t
 
 DAE
-y5 = y2/y4;
+y5 = y2*y4;
 '''
 
 h = 0.1
@@ -28,7 +28,7 @@ y1.append(1)
 y2.append(1)
 y3.append(0)
 y4.append(1)
-y5.append(y2[-1]/y4[-1])
+y5.append(y2[-1]*y4[-1])
 
 steps = round(tmax/h)
 
@@ -41,7 +41,7 @@ for i in range(1,steps+1):
     y2.append(y2[-1])
     y3.append(y3[-1])
     y4.append(y4[-1])
-    y5.append(y2[-1]/y4[-1])
+    y5.append(y2[-1]*y4[-1])
 
     DY1 = []
     DY2 = []
@@ -66,7 +66,7 @@ for i in range(1,steps+1):
     DY2.append(h*(-1*DY3[0]))
     DY3.append(h*(1*DY2[0]))
     DY4.append(h*(1*DY4[0]))
-    DY5.append((1/DY4[0])*(DY2[k] - DY5[0]*DY4[k]))
+    DY5.append(DY2[k]*DY4[0] + DY2[0]*DY4[k])
 
     y1[-1] = y1[-1] + DY1[-1]
     y2[-1] = y2[-1] + DY2[-1]
@@ -85,17 +85,17 @@ for i in range(1,steps+1):
         DY3.append((h/k)*(1*DY2[k-1]))
         DY4.append((h/k)*(1*DY4[k-1]))
         
-        l = 1
-        m = k-1
-        mixed = 0
-        for r in range(1,k+1):
+        l = k
+        m = 0
+        sum = 0
+        for r in range(0,k+1):
             #print(f'{r}: m {m} l {l}')
-            mixed = mixed + (DY5[m]*DY4[l])
+            sum = sum + (DY2[l]*DY4[m])
             #print(f'{mixed}')
-            l = l+1
-            m = m-1
+            l = l-1
+            m = m+1
         
-        DY5.append((1/DY4[0])*(DY2[k]-mixed))
+        DY5.append(sum)
 
         y1[-1] = y1[-1] + DY1[-1]
         y2[-1] = y2[-1] + DY2[-1]
